a708b41bec86b520e4c69bc10e7748aee363d77a
Satriadinata <satriadinata2@gmail.com>
2021-01-11 22:00:16 +0700

    Update Data

--- app/Http/Controllers/Admin/TravelPackageController.php
+++ app/Http/Controllers/Admin/TravelPackageController.php
@@ -68,7 +68,10 @@ class TravelPackageController extends Controller
      */
     public function edit($id)
     {
-        //
+        $item = TravelPackage::findOrFail($id);
+        return view('pages.admin.travel-package.edit',[
+            'item' => $item
+        ]);
     }
 
     /**
@@ -78,9 +81,15 @@ class TravelPackageController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(TravelPackageRequest $request, $id)
     {
-        //
+        
+        $data = $request->all();
+        $data['slug'] = Str::slug($request->title);
+
+        $item= TravelPackage::findOrFail($id);
+        $item->update($data);
+        return redirect()->route('travel-package.index');
     }
 
     /**
