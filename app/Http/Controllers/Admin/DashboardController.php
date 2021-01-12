7eb946a7c82f2f3470fcbb82fab250aae5b62785
Satriadinata <satriadinata2@gmail.com>
2021-01-12 10:35:21 +0700

    dashboard update

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\TravelPackage;
+use App\Models\Transaction;
 class DashboardController extends Controller
 {
     //
     public function index(){
-        return view('pages.admin.dashboard');
+       
+        return view('pages.admin.dashboard',[
+            'travel_packages' => TravelPackage::count(),
+            'transaction' => Transaction::count(),
+            'transaction_pending' => Transaction::where('transaction_status','PENDING')->count(),
+            'transaction_success' => Transaction::where('transaction_status','SUCCESS')->count(),
+        ]);
     }
 }
